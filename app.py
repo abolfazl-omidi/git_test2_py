432c3f24c45497152bda4bfaa203b633ed2c5596
abolfazl <abolfazlomidi006@gmail.com>
2025-09-29 12:17:00 +0330

    added for

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ def sayHello(name):
     print(f"hello and wellcom {name}")
 
 sayHello('omidi')
+
+names = ['odis' , 'ava' , 'omidi' , 'amir']
+for i in names : 
+    print(i)
+    print('hello world')
